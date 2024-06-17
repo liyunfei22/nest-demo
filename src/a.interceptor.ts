f78e814f1e1cdda3a1d7753b251ea6b206fd9817
liyunfei02 <liyunfei02@daojia-inc.com>
2024-06-17 16:42:49 +0800

    feat: 拦截器

---
+++ src/a.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+  data: T;
+}
+
+@Injectable()
+export class AInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    console.log('Before...');
+    return next.handle().pipe(map((data) => ({ data })));
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  UseInterceptors,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AInterceptor } from 'src/a.interceptor';
 
 @Controller('user')
+@UseInterceptors(AInterceptor)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 

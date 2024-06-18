3108044834085131884238571291b69016c69a21
liyunfei02 <liyunfei02@daojia-inc.com>
2024-06-18 16:16:34 +0800

    feat: jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, Res, Session, Headers, UnauthorizedException } from '@nestjs/common';
 import { AppService } from './app.service';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
+  @Inject(JwtService)
+  private readonly jwtService: JwtService;
+
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('sss')
+  getHello(@Session() session): string {
+    console.log(session);
+    session.count = session.count ? session.count + 1 : 1;
+    return session.count;
+  }
+  @Get('ttt')
+  ttt(
+    @Headers('authorization') authorization: string,
+    @Res({ passthrough: true }) response: Response,
+  ): string {
+    console.log('authorization', authorization);
+    if (authorization) {
+      console.log('authorization', authorization);
+      try {
+        const token = authorization.split(' ')[1];
+        const data = this.jwtService.verify(token);
+        console.log(data)
+        const newToken = this.jwtService.sign({
+          count: data.count + 1,
+        });
+        response.setHeader('token', newToken);
+        return data.count + 1;
+      } catch (e) {
+        console.log(e);
+        throw new UnauthorizedException();
+      }
+    }
+    const newToken = this.jwtService.sign({
+      count: 1,
+    });
+
+    response.setHeader('token', newToken);
+    return 'hello1';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: 'li',
+      signOptions: {
+        expiresIn: '7d',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
